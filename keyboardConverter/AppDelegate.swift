ff0a49df3f7f981799d5fa6a04b224fdebc36a3b
Dongho Kim <k45414254@gmail.com>
2021-07-18 21:11:32 +0900

    Initial Commit

---
+++ keyboardConverter/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  keyboardConverter
+//
+//  Created by 김동호 on 2021/07/18.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
